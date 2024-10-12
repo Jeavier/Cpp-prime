df6e13669b8395b94c22ad741661927775a319a5
Jeavier <143787518+Jeavier@users.noreply.github.com>
2024-10-12 18:52:15 +0530

    Add files via upload

--- DeciToBinary.cpp
+++ DeciToBinary.cpp
@@ -1,49 +1,21 @@
-// #include<iostream>
-
-// using namespace std;
-
-// int main(){
-//     int n;
-//     cin >> n;
-
-//     int ans = 0;
-//     int i = 1;
-
-//     while(n != 0){
-//         int bit = n & 1;
-//         ans = (bit * i) + ans;
-//         n = n>>1;
-//         i = i*10;   
-//     }
-//     cout<<"Answer is  "<<ans<<endl;
-//     return 0;   
-// }
-
 #include<iostream>
 using namespace std;
 
-int main() {
+int main(){
     int n;
-    cin >> n;
+    cin>>n;
 
-    bool isNegative = (n < 0);
-    n = abs(n);  // Take absolute value for binary conversion
+    int ans=0;
+    int pow=1;
 
-    int ans = 0;
-    int i = 1;
+    while(n>0){
+        int paritydigit = n%2;
+        ans += (paritydigit*pow);
+        pow*=10;
+        n/=2;
 
-    while(n != 0){
-        int bit = n & 1;
-        ans = (bit * i) + ans;
-        n = n >> 1;
-        i = i * 10;
     }
-
-    if (isNegative) {
-        cout << "Answer (for absolute value) is -" << ans << endl;
-    } else {
-        cout << "Answer is " << ans << endl;
-    }
+    cout<<ans<<endl;
 
     return 0;
 }
