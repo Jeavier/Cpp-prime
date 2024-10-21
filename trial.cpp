75dee8983d01f215aece1a70f428d2098d3f581a
Jeavier <143787518+Jeavier@users.noreply.github.com>
2024-10-21 19:26:55 +0530

    Add files via upload

---
+++ trial.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // float x,n;
+    // cout<<"enter a num = ";
+    // cin>>x;
+    // cout<<"enter other num = ";
+    // cin>>n;
+
+    // float b= 6.6/x+2*n;
+
+    // cout<<b;
+
+    float b= 7/22 *(3.14+2) *3/5;
+
+    cout<<b;
+}
