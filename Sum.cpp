b0e01a045520a9aa4a02f69a0f9afb392956be33
Jeavier <143787518+Jeavier@users.noreply.github.com>
2024-10-22 18:39:30 +0530

    Add files via upload

---
+++ Sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float x,y;
+    cout<<"enter 1st number = ";
+    cin>>x;
+
+    cout<<"enter 2nd number = ";
+    cin>>y;
+
+    cout<<"Sum is = "<<x+y<<endl;
+}
